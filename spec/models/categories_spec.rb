dae6fd390e6197f074e91f76e784b2d211a9ebfa
Don Forrest <forrest@philippinedev.com>
2023-02-20 15:41:51 +0800

    Add category associations validation

--- spec/models/categories_spec.rb
+++ spec/models/categories_spec.rb
@@ -1,6 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Category, type: :model do
+  describe "associations" do
+    it { should have_many(:products) }
+  end
+
   describe "validations" do
     it { should validate_presence_of(:name) }
   end
