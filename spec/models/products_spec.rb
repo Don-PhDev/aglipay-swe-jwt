a8d51f5c571a081a057a455376f8a7cfe010c5e0
Don Forrest <forrest@philippinedev.com>
2023-02-20 15:35:29 +0800

    Add products associations validation

--- spec/models/products_spec.rb
+++ spec/models/products_spec.rb
@@ -1,1 +1,8 @@
 require "rails_helper"
+
+RSpec.describe Product, type: :model do
+  describe 'associations' do
+    it { should belong_to(:category) }
+    it { should have_many(:orders) }
+  end
+end
