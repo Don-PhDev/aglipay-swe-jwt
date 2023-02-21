f3b6105f7cae8c02a52ae4bd8be27bd3b60c63d8
Don Forrest <forrest@philippinedev.com>
2023-02-21 11:12:14 +0800

    Use gon gem for js in ActiveAdmin

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -2,4 +2,12 @@ ActiveAdmin.register Order do
   menu priority: 3
 
   permit_params :quantity, :total_amount, :user_id, :product_id
+
+  controller do
+    before_action :load_products, only: [:new, :create, :edit, :update]
+
+    def load_products
+      gon.products = Product.all.select(:id, :name, :price)
+    end
+  end
 end
