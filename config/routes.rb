6edb82c9abf5dbcebf6681d6987a748f1ced6818
Don Forrest <forrest@philippinedev.com>
2023-02-22 17:56:31 +0800

    Add routes for devise users controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
 
   namespace :api do
     namespace :v1 do
