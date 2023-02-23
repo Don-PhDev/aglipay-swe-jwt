f0e35a73cca2fe13cffac6155cc7f8ac7279fcb1
Don Forrest <forrest@philippinedev.com>
2023-02-23 11:51:45 +0800

    Rescue ActiveRecord::RecordNotUnique

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from ActiveRecord::RecordNotUnique, with: :record_not_unique
+
+  private
+
+  def record_not_unique
+    render json: { error: 'Email has already been taken' }, status: :unprocessable_entity
+  end
 end
