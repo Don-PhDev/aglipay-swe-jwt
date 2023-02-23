aa07cdfef0d98a2923aee6ad90855a03b4c7e6a2
Don Forrest <forrest@philippinedev.com>
2023-02-23 10:13:26 +0800

    Add seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+def seeder
+  AdminUser.create!(
+    email: "admin@example.com",
+    password: "password",
+    password_confirmation: "password"
+  ) if Rails.env.development?
+
+  User.create!(
+    email: "example@gmail.com",
+    password: "password",
+    password_confirmation: "password"
+  )
+
+  10.times do
+    Category.create!(
+      name: Faker::Commerce.unique.department
+    )
+  end
+
+  30.times do
+    Product.create!(
+      category_id: Category.all.pluck(:id).sample,
+      name: Faker::Commerce.product_name,
+      price: Faker::Commerce.price.to_d,
+    )
+  end
+
+  10.times do
+    Order.create!(
+      user_id: User.all.pluck(:id).sample,
+      product_id: Product.all.pluck(:id).sample,
+      quantity: rand(1...100).to_d
+    )
+  end
+end
+
+seeder
+
+puts '-------------------------------'
+puts 'Seed results:'
+puts '-------------------------------'
+puts "Users: #{User.count}"
+puts "Categories: #{Category.count}"
+puts "Products: #{Product.count}"
+puts "Orders: #{Order.count}"
