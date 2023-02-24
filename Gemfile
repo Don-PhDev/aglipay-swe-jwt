8b3f294c649bb58d2b859cbace389c5399260550
Don Forrest <forrest@philippinedev.com>
2023-02-24 10:19:59 +0800

    Add json method, require all support files, include ApiHelpers, and organize gems

--- Gemfile
+++ Gemfile
@@ -57,8 +57,6 @@ gem "sassc-rails"
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
-  gem "factory_bot_rails"
-  gem "faker"
   gem "pry-byebug"
   gem "rspec-rails"
 end
@@ -75,6 +73,8 @@ group :development do
 end
 
 group :test do
+  gem "factory_bot_rails"
+  gem "faker"
   gem "shoulda-matchers", "~> 5.3"
   gem "database_cleaner-active_record"
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,7 +23,7 @@ require_relative './support/shoulda_matchers'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -67,4 +67,6 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   config.include Devise::Test::IntegrationHelpers, type: :request
+
+  config.include ApiHelpers
 end
